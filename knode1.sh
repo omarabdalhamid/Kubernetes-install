14bc7ee97b94d591b34f5170cf85c255eeb2caa5
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2019-09-13 13:25:04 +0200

    Update and rename knode.sh to knode1.sh

--- knode1.sh
+++ knode1.sh
@@ -14,10 +14,39 @@ apt install ntp -y
 service ntp start
 systemctl enable ntp
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - \
-    && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable" \
-    && sudo apt-get update \
-    && sudo apt-get install docker-ce=18.03.1~ce-0~ubuntu -yq
+# Install Docker CE
+## Set up the repository:
+### Install packages to allow apt to use a repository over HTTPS
+
+### Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+### Add Docker apt repository.
+add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+## Install Docker CE.
+apt-get update && apt-get install docker-ce=18.06.2~ce~3-0~ubuntu
+
+# Setup daemon.
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart docker.
+systemctl daemon-reload
+systemctl restart docker
 
 sudo add-apt-repository universe -y
 
