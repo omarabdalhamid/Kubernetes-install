85647ff6e529172c79a9fa1c591c554170576bf5
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2019-11-10 23:47:18 +0200

    Update kmaster.sh

--- kmaster.sh
+++ kmaster.sh
@@ -35,6 +35,24 @@ service docker start
 
 systemctl enable docker 
 
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart docker.
+systemctl daemon-reload
+systemctl restart docker
+
+
 apt-get update && apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
